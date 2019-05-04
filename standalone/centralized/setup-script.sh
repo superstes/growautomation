93d227f87bf846c19d84dcbc38cf0794ca4e31ba
growautomation-at <50269951+growautomation-at@users.noreply.github.com>
2019-05-04 18:28:22 +0200

    Update setup-script.sh

--- standalone/centralized/setup-script.sh
+++ standalone/centralized/setup-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo apt-get update && apt-get upgrade && apt-get dist-upgrade
-sudo apt-get install python3
-sudo apt-get install python3-pip python3-dev mysql-server mysql-client git
-sudo python3 -m pip install mysql-connector-python pyserial Adafruit_DHT RPi.GPIO spidev
-sudo mkdir -p /etc/growautomation-at/download 
+apt-get update && apt-get upgrade && apt-get dist-upgrade
+apt-get install python3 python3-pip python3-dev mysql-server mysql-client git
+python3 -m pip install mysql-connector-python pyserial Adafruit_DHT RPi.GPIO spidev
+mkdir -p /etc/growautomation-at/download 
 cd !$
-sudo git clone https://github.com/growautomation-at/standalone-controller.git
+git clone https://github.com/growautomation-at/standalone-controller.git
+mysql_secure_installation
+#Note: This setup is not finished!
