753a9fd11d9b84ee1e3341eb46b45238c0056960
Rene Rath <rene.rath@growautomation.at>
2020-04-06 23:16:07 +0200

    fixed threaded timer loop

--- code/agent/service/looper.py
+++ code/agent/service/looper.py
@@ -1,11 +1,7 @@
 # source: https://github.com/sankalpjonn/timeloop
 # modified for use in ga
 
-
 from threading import Thread, Event
-from signal import signal
-from signal import SIGINT
-from signal import SIGTERM
 from time import sleep as time_sleep
 from datetime import timedelta
 
@@ -15,63 +11,60 @@ from ga.core.ant import LogWrite
 class Job(Thread):
     def __init__(self, interval, execute, *args, **kwargs):
         Thread.__init__(self)
-        self.stopped = Event()
-        self.interval = timedelta(seconds=interval)
+        self.state_stop = Event()
+        self.interval = interval
         self.execute = execute
         self.args = args
         self.kwargs = kwargs
 
     def stop(self):
-        self.stopped.set()
+        self.state_stop.set()
         self.join()
+        LogWrite("Thread of function  '%s' stopped" % self.execute.__name__, loglevel=3)
 
     def run(self):
-        while not self.stopped.wait(self.interval.total_seconds()):
+        LogWrite("Thread of function  '%s' started" % self.execute.__name__, loglevel=4)
+        while not self.state_stop.wait(self.interval.total_seconds()):
             self.execute(*self.args, **self.kwargs)
+            if self.state_stop.isSet():
+                LogWrite("Exiting thread of function '%s'" % self.execute.__name__, loglevel=4)
+                break
 
 
 class Loop:
     def __init__(self):
         self.jobs = []
 
-    def job(self, time):
-        def decorator(f):
-            self.add_job(f, time)
-            return f
+    def start(self, deamon=True):
+        if deamon is False:
+            LogWrite("Starting threads in foreground", loglevel=3)
+        else:
+            LogWrite("Starting threads in background", loglevel=3)
+        for j in self.jobs:
+            j.daemon = deamon
+            j.start()
+
+        if not deamon:
+            self.block()
+
+    def job(self, sleeptime):
+        def decorator(function):
+            self.add_job(function, timedelta(seconds=sleeptime))
+            return function
         return decorator
 
-    def add_job(self, func, interval, *args, **kwargs):
-        self.jobs.append(Job(interval, func, *args, **kwargs))
+    def add_job(self, function, sleep, *args, **kwargs):
+        self.jobs.append(Job(sleep, function, *args, **kwargs))
 
     def block(self):
-        signal(SIGTERM, self.exit("term"))
-        signal(SIGINT, self.exit("int"))
-
         while True:
             try:
                 time_sleep(1)
-            except SystemExit:
+            except:
                 self.stop()
-                break
-
-    def start(self, block=False):
-        LogWrite("Starting Timeloop")
-        for j in self.jobs:
-            j.daemon = not block
-            j.start()
-            LogWrite("Registered job {}".format(j.execute))
-
-        LogWrite("Timeloop now started. Jobs will run based on the interval set")
-        if block:
-            self.block()
+                raise SystemExit
 
     def stop(self):
-        LogWrite("Timeloop exited")
         for j in self.jobs:
-            LogWrite("Stopping job {}".format(j.execute))
             j.stop()
-        return SystemExit
-
-    def exit(self, reason):
-        LogWrite("Shutting down because of sig%s interrupt" % reason)
-        raise SystemExit
+        LogWrite("All threads stopped. Exiting loop", loglevel=2)
