20e9d08c0d2261341415661e669df5787b78a09c
Rene Rath <rene.rath@growautomation.at>
2020-04-08 22:08:07 +0200

    added run-once thread

--- code/agent/service/threader.py
+++ code/agent/service/threader.py
@@ -1,35 +1,44 @@
 # source: https://github.com/sankalpjonn/timeloop
 # modified for use in ga
 
-from threading import Thread, Event
-from time import sleep as time_sleep
-from datetime import timedelta
+# ga_version0.3
 
 from ga.core.ant import LogWrite
 
+from threading import Thread, Event
+from time import sleep as time_sleep
+from datetime import timedelta
+from inspect import getfile as inspect_getfile
+from inspect import currentframe as inspect_currentframe
+
+LogWrite("Current module: %s" % inspect_getfile(inspect_currentframe()), level=2)
+
 
 class Job(Thread):
-    def __init__(self, interval, execute, name, *args, **kwargs):
+    def __init__(self, interval, execute, name, run_once=False):
         Thread.__init__(self)
         self.state_stop = Event()
         self.interval = interval
         self.execute = execute
-        self.args = args
-        self.kwargs = kwargs
         self.name = name
+        self.run_once = run_once
 
     def stop(self):
         self.state_stop.set()
         self.join()
-        LogWrite("Thread of function  '%s' stopped" % self.execute.__name__, loglevel=3)
+        LogWrite("Thread of function  '%s' stopped" % self.execute.__name__, level=3)
 
     def run(self):
-        LogWrite("Thread of function  '%s' started for '%s'" % (self.execute.__name__, self.name), loglevel=4)
-        while not self.state_stop.wait(self.interval.total_seconds()):
-            self.execute(*self.args, **self.kwargs)
-            if self.state_stop.isSet():
-                LogWrite("Exiting thread of function '%s'" % self.execute.__name__, loglevel=4)
-                break
+        LogWrite("Thread of function  '%s' started for '%s'" % (self.execute.__name__, self.name), level=4)
+        if self.run_once:
+            self.execute()
+            Loop.stop_thread(self.name)
+        else:
+            while not self.state_stop.wait(self.interval.total_seconds()):
+                self.execute()
+                if self.state_stop.isSet():
+                    LogWrite("Exiting thread of function '%s'" % self.execute.__name__, level=4)
+                    break
 
 
 class Loop:
@@ -38,9 +47,9 @@ class Loop:
 
     def start(self, daemon=True, single_thread=None):
         if daemon is False:
-            LogWrite("Starting threads in foreground", loglevel=3)
+            LogWrite("Starting threads in foreground", level=3)
         else:
-            LogWrite("Starting threads in background", loglevel=3)
+            LogWrite("Starting threads in background", level=3)
             for job in self.jobs:
                 if single_thread is not None:
                     if job.name == single_thread:
@@ -52,9 +61,13 @@ class Loop:
         if not daemon:
             self.block_root_process()
 
-    def thread(self, sleep_time, thread_name, *args, **kwargs):
+    def thread(self, sleep_time, thread_name):
         def decorator(function):
-            self.jobs.append(Job(timedelta(sleep_time), function, thread_name, *args, **kwargs))
+            if sleep_time == 0:
+                sleep_time_new = 600
+                self.jobs.append(Job(timedelta(seconds=sleep_time_new), function, thread_name, run_once=True))
+            else:
+                self.jobs.append(Job(timedelta(seconds=sleep_time), function, thread_name))
             return function
         return decorator
 
@@ -69,7 +82,7 @@ class Loop:
     def stop(self):
         for job in self.jobs:
             job.stop()
-        LogWrite("All threads stopped. Exiting loop", loglevel=2)
+        LogWrite("All threads stopped. Exiting loop", level=2)
 
     def stop_thread(self, thread_name):
         to_process_list = self.jobs
@@ -77,11 +90,11 @@ class Loop:
             if job.name == thread_name:
                 job.stop()
                 self.jobs.remove(job)
-                LogWrite("Thread %s stopped." % job.name, loglevel=2)
+                LogWrite("Thread %s stopped." % job.name, level=2)
 
-    def reload_thread(self, sleep_time, thread_name, *args, **kwargs):
+    def reload_thread(self, sleep_time, thread_name):
         self.stop_thread(thread_name)
-        self.thread(sleep_time, thread_name, *args, **kwargs)
+        self.thread(sleep_time, thread_name)
         self.start(single_thread=thread_name)
 
     def list(self):
