b2b3b7a44c8eeceba67cb71119460b27cd2a2d5b
Rene Rath <rene.rath@growautomation.at>
2020-04-12 21:47:43 +0200

    format fetchone tuple to string

--- code/agent/core/owl.py
+++ code/agent/core/owl.py
@@ -121,7 +121,7 @@ class DoSql:
                 @lru_cache()
                 def readcache(doit):
                     cursor.execute(doit)
-                    return False if cursor.rowcount < 0 else cursor.fetchone() if cursor.rowcount == 1 else cursor.fetchall()
+                    return False if cursor.rowcount < 0 else cursor.fetchone()[0] if cursor.rowcount == 1 else cursor.fetchall()
                 data = readcache(command)
             else:
                 cursor.execute(command)
