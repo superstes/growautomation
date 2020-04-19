c536e2c1af0efd7b4814f844ede78329c3385bd4
Rene Rath <rene.rath@growautomation.at>
2020-04-19 23:24:45 +0200

    added core functionality to sensor master class

--- code/agent/core/snake.py
+++ code/agent/core/snake.py
@@ -19,6 +19,7 @@
 #     Web: https://git.growautomation.at
 
 # ga_version 0.3
+# sensor master module
 
 from inspect import getfile as inspect_getfile
 from inspect import currentframe as inspect_currentframe
@@ -26,9 +27,11 @@ from sys import argv as sys_argv
 from subprocess import Popen as subprocess_popen
 from subprocess import PIPE as subprocess_pipe
 import signal
+from time import sleep as time_sleep
 
 from ga.core.config import Config
 from ga.core.owl import DoSql
+from ga.core.owl import sql_replace
 from ga.core.ant import LogWrite
 from ga.core.owl import debugger
 
@@ -38,107 +41,145 @@ LogWrite("Current module: %s" % inspect_getfile(inspect_currentframe()), level=2
 class Balrog:
     def __init__(self, sensor):
         self.sensor = sensor
-        self.processed_list = []
+        self.processed_list, self.lock_list = [], []
         signal.signal(signal.SIGTERM, self.stop)
         signal.signal(signal.SIGINT, self.stop)
-        self.devicetype() if self.sensor in Config(output="name", table="object", filter="class = 'sensor'").get() else self.device(self.sensor)
+        self.sensor_type = True if self.sensor in Config(output="name", table="object", filter="class = 'sensor'").get() else False
+        self.devicetype() if self.sensor_type is True else self.device(self.sensor)
 
     def devicetype(self):
         device_list = []
         for device in Config(output="name", table="object", filter="class = '%s'" % self.sensor).get():
-            if not Config(setting="timer", belonging=device).get(): device_list.append(device)
-        [self.device(device) for device in device_list]
+            if not Config(setting="timer", belonging=device).get() and Config(setting="enabled", belonging=device).get() == "1":
+                device_list.append(device)
+        for device in device_list:
+            if device in self.processed_list: continue
+            else: self.device(device)
         # ask for threading support when sensor-functions are added
 
     def device(self, device):
-        if device in self.processed_list: return False
+        # if device => self.sensor doesn't need to check if enabled since it was already checked when accepting the timer
+        connection = Config(setting="connection", belonging=device).get()
+        if connection == "direct":
+            output = self.start(device)
+            debugger("snake - device |direct |output '%s'" % output)
+            self.write_data(device, output)
+        elif connection == "downlink": self.downlink(device, Config(setting="downlink", belonging=device).get())
         else:
-            connection = Config(setting="connection", belonging=device).get()
-            if connection == "direct":
-                function = Config(setting="function", belonging=Config(output="class", table="object", setting=device).get())
-                output, error = subprocess_popen(["/usr/bin/python3 %s %s %s" % (function, Config(setting="port", belonging=device).get(), device)],
-                                                 shell=True, stdout=subprocess_pipe, stderr=subprocess_pipe).communicate()
-                output_str, error_str = output.decode("ascii"), error.decode("ascii")
-            elif connection == "downlink":
-                self.downlink(device, Config(setting="downlink", belonging=device).get())
-            else: return False
+            debugger("snake - device |%s has no connection configured" % device)
+            LogWrite("Device %s has no acceptable connection configured" % device, level=1)
+            return False
 
     def downlink(self, device, downlink):
-        def start(setting, data):
-            # start downlink function with downlink settings and data (if needed)
+        if Config(setting="enabled", belonging=downlink).get() != "1": return False
+        devicetype = Config(output="class", table="object", setting=downlink).get()
+        if Config(setting="enabled", belonging=devicetype).get() != "1": return False
+        type_opp = Config(setting="output_per_port", belonging=devicetype).get()
 
-        downlink_type = Config(output="class", table="object", setting=downlink).get()
-        downlink_type_opp = Config(setting="output_per_port", belonging=downlink_type).get()
-        if downlink_type_opp == "1":
-            # get downlink setting and start()
-        elif downlink_type_opp == "0":
-            # check if self.sensor is devicetype -> yes check if other devices are connected to the same downlink type (minus processed list)
-            # add those devices + their downlink ports to a dict and add to self.processed list
-            # funny stuff for str/list/dict -> define default input/output for sensor-/downlink functions!!
-            # start()
+        def get_setting_dict():
+            type_setting, setting = Config(output="setting,data", belonging=devicetype), Config(output="setting,data", belonging=downlink)
+            setting_dict, type_setting_dict = {}, {}
+            for setting, data in setting: setting_dict[setting] = data
+            for setting, data in type_setting:
+                if setting not in setting_dict.keys(): setting_dict[setting] = data
+            debugger("snake - downlink |get_setting_dict |output '%s'" % setting_dict)
+            return setting_dict
+
+        if type_opp == "1":
+            sensor_dict = {device: Config(setting="port", belonging=device).get()}
+            debugger("snake - downlink |opp 1 |input '%s'" % sensor_dict)
+            output = self.start(downlink, device_mapping_dict=sensor_dict, setting_dict=get_setting_dict())
+            debugger("snake - downlink |opp 1 |output '%s'" % output)
+        elif type_opp == "0":
+            portcount = Config(setting="portcount", belonging=downlink)
+            if self.sensor_type:
+                sensor_list, sensor_dict = Config(output="name", setting="downlink", filter="data = '%s'" % downlink).get(), {}
+                for sensor in sensor_list:
+                    if Config(output="class", table="object", setting=sensor).get() == self.sensor:
+                        if sensor in self.processed_list or Config(setting="enabled", belonging=sensor).get() != "1": continue
+                        sensor_dict[sensor] = Config(setting="port", belonging=sensor)
+            else:
+                sensor_dict = {device: Config(setting="port", belonging=device).get()}
+            if len(sensor_dict.keys()) < portcount:
+                for i in range(portcount):
+                    if i not in sensor_dict.values():
+                        sensor_dict["dummy_%s" % i] = i
+                        # portlist in dict will start at 0 => throw it into the docs
+                sensor_dict = {key: value for key, value in sorted(sensor_dict.items(), key=lambda item: item[1])}
+            debugger("snake - downlink |opp 0 |input '%s'" % sensor_dict)
+            output_dict = self.start(downlink, device_mapping_dict=sensor_dict, setting_dict=get_setting_dict())
+            debugger("snake - downlink |opp 0 |output '%s'" % output_dict)
+            for sensor, data in output_dict.items():
+                if sensor in self.processed_list: self.write_data(sensor, data)
+            # if opp -> output must be string
+            # if not opp -> output must be dict with sensor as key and data as value
         else: return False
 
-        # settings
-        # portcount int,  bool, output_format str;list;dict, output_format_delimeter
-        # downlink = downlink device
-        # downlink function:
-        # for device append downlink to list
-        # for dl in downlinklist
-        #   check if device is enabled
-        #   get devicetype -> append to downlinktypelist
-        # for downlinktype in downlinktypelist
-        #   get typesettings
-        #   check if type is enabled
-        #
-        # for dl in downlinklist
-        #   get devicesettings
-        #   overwrite typesettings with devicesettings
-        #   each device on downlink
-        #     add portdict[port]=device_name
-        #     compare to portcount of downlink -> add dev-null members for ports which are not in use
-        # get data from downlink function -> sensor function(downlinksetting_dict, portdict)
-        # zip the
+    def start(self, device, device_mapping_dict=None, setting_dict=None):
+        if device_mapping_dict is None: self.processed_list.extend(device)
+        else:
+            for device in device_mapping_dict.keys():
+                if device.find("dummy") == -1: self.processed_list.append(device)
+        self.lock(device)
+        function = Config(setting="function", belonging=Config(output="class", table="object", setting=device).get()).get()
+        devicetype_class = Config(output="class", table="object", setting=Config(output="class", table="object", setting=device).get()).get()
+        function_path = "%s/%s/%s" % (Config(setting="root_path").get(), devicetype_class, function)
+        LogWrite("Starting function %s for device %s.\nInput data:\nDevice mapping '%s'\nSettings '%s'" % (function_path, device, device_mapping_dict, setting_dict), level=4)
+        output, error = subprocess_popen(["/usr/bin/python3 %s %s %s %s" % (function_path, Config(setting="port", belonging=device).get(), device_mapping_dict, setting_dict)],
+                                         shell=True, stdout=subprocess_pipe, stderr=subprocess_pipe).communicate()
+        output_str, error_str = output.decode("ascii").strip(), error.decode("ascii").strip()
+        LogWrite("Function '%s' was processed for device %s.\nOutput '%s'" % (function_path, device, output_str), level=3)
+        debugger("snake - start |output by processing %s '%s'" % (device, output_str))
+        if error_str != "":
+            LogWrite("Error by executing %s:\n'%s'" % (device, error_str), level=2)
+            debugger("snake - start |error by executing %s '%s'" % (device, error_str))
+        if device_mapping_dict is None: return output_str
+        else: return dict(output_str)
 
-    def database(self):
-        print("write data from device or downlink function into db")
+    def lock(self, device):
+        try_count, wait_time, max_try_count = 1, 10, 31
+        # note: set config for wait time and timeout in db belonging to sensor_master
+        while True:
+            if Config(setting="lock", belonging=device, table="tmp").get() == "1": time_sleep(wait_time)
+            else: break
+            if try_count > max_try_count:
+                debugger("snake - lock |device %s reached max retries -> giving up to get lock" % device)
+                LogWrite("Unable to get lock for device '%s' in time. Timeout (%s sec) reached." % (device, wait_time * try_count), level=2)
+                return False
+            debugger("snake - lock |device %s waiting for lock for % seconds" % (device, wait_time * try_count))
+            try_count += 1
+        try_count -= 1
+        data_dict = {"data": "1", "author": "sensor_master", "belonging": Config("hostname").get(), "setting": "lock"}
+        sql_replace(data_dict, table="tmp")
+        self.lock_list.append(device)
+        debugger("snake - lock |device %s locked" % device)
+        LogWrite("Locked device '%s' (waited for ~%s sec)." % (device, wait_time * try_count), level=2)
+        return True
 
-    def stop(self):
-        print("finish whatever you're doing and go die")
+    def unlock(self, device):
+        data_dict = {"data": "0", "author": "sensor_master", "belonging": device, "setting": "lock"}
+        sql_replace(data_dict, table="tmp")
+        self.lock_list.remove(device)
+        debugger("snake - unlock |device %s unlocked" % device)
+        LogWrite("Unlocked device '%s'." % device, level=2)
+        return True
+
+    def write_data(self, device, data):
+        sql = DoSql("INSERT INTO ga.Data (agent,data,device) VALUES ('%s','%s','%s');" % (Config("hostname").get(), data, device), write=True).start()
+        LogWrite("Wrote data for device '%s' to database. Output: %s" % (device, sql), level=4)
+        return sql
+
+    def stop(self, signum=None, stack=None):
+        if signum is not None:
+            debugger("snake - stop |got signal %s" % signum)
+            LogWrite("Sensor master received signal %s" % signum, level=2)
+        if len(self.lock_list) > 0:
+            unlock_list = self.lock_list
+            for device in unlock_list: self.unlock(device)
+            debugger("snake - stop |unlocked following devices '%s'" % unlock_list)
+        debugger("snake - stop |exiting")
         raise SystemExit
 
-# random settings
-
-# class old:
-    # will not be needed often -> should be queried manually if needed
-    # namemaxletters = 10
-    # namemaxnumbers = 100
-    # sensortime = "10"		        #How often should the sensordata be written to the database (minutes)
-    # sensorahtdisabled = "no"
-    # sensorehdisabled = "no"
-    # adcdisabled = ["adc02"]
-    # adcconnected = {"adc01": "i2c-1", "adc02": "i2c-2"}
-    # ahtadisabled = ["ahta02"]
-    # ahtaconnected = {"ahta01": "26", "ahta02": "19"}
-    # ehbdisabled = ["ehb02"]
-    # ehbconnected = {"ehb01": {"adc01": "0"}, "ehb02": {"adc02": "1"}}
-    # actiontypes = {"eh": ("pump", "win"), "aht": ("win")}
-    # actionblock01 = {"sensor": {"eh": ("ehb01", "ehb02"), "aht": ("ahta01", "ahta02")}, "action": {"win": ("wina02")}}
-    # actionblock02 = {"sensor": {"eh": ("ehb01", "ehb02")}, "action": {"pump": ("pumpa01", "pumpa04"), "win": ("wina01")}}
-    # pumpdisabled = []
-    # pumpconnected = ("pumpa01", "pumpa04")
-    # pumpactivation = "60"		#The pump will be activated if the humidity falls under this value
-    # pumptime = 10 #600		    #Runtime in seconds
-    # windisabled = {}
-    # winconnected = {"win01": {"fwd": "20", "rev": "21"}, "win02": {"fwd": "16", "rev": "12"}}
-    # winopentime = 5
-    # psua01password = "PASSWORD"
-    # psua01ip = "IP"
-    # psua01webport = 8080
-    # psua01outlets = {"1": "pumpa01", "2": "pumpa02", "3": "pumpa03", "4": ""}
-    # psua02password = "PASSWORD"
-    # psua02ip = "IP"
-    # psua02webport = 8080
-    # psua02outlets = {"1": "pumpa04", "2": "", "3": "", "4": ""}
 
 try: Balrog(sys_argv[1])
 except IndexError:
