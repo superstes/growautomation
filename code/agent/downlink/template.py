eae805ccc7383ebf46bbf186667f27171c783788
Rene Rath <rene.rath@growautomation.at>
2020-04-26 19:26:27 +0200

    added template file -> will be populated soon

---
+++ code/agent/downlink/template.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# This file is part of Growautomation
+#     Copyright (C) 2020  René Pascal Rath
+#
+#     Growautomation is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#     E-Mail: rene.rath@growautomation.at
+#     Web: https://git.growautomation.at
+
+# ga_version 0.3
+
+from ga.core.config import Config
+from ga.core.ant import LogWrite as LogModule
+from sys import argv as sys_argv
+
+
+def LogWrite(log: str, level=3):
+    LogModule(log, typ="check", level=level)
+
+# get portdict or port
+# dl funcationality -> get data
+# if outputtype = dict -> add dl data to dict
+# if outputtype = str -> only output data for the (input) port
