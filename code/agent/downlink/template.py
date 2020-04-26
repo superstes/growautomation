4846dd6850ac87256dbd78f795e380254a252ce0
Rene Rath <rene.rath@growautomation.at>
2020-04-26 22:53:09 +0200

    updated template

--- code/agent/downlink/template.py
+++ code/agent/downlink/template.py
@@ -20,15 +20,61 @@
 
 # ga_version 0.3
 
-from ga.core.config import Config
 from ga.core.ant import LogWrite as LogModule
+from ga.core.smallant import debugger
+
+from inspect import getfile as inspect_getfile
+from inspect import currentframe as inspect_currentframe
 from sys import argv as sys_argv
 
 
 def LogWrite(log: str, level=3):
-    LogModule(log, typ="check", level=level)
+    LogModule(log, typ="downlink", level=level)
 
-# get portdict or port
-# dl funcationality -> get data
-# if outputtype = dict -> add dl data to dict
-# if outputtype = str -> only output data for the (input) port
+
+function = inspect_getfile(inspect_currentframe())
+LogWrite("Current module: %s" % function, level=2)
+
+try:
+    argument = sys_argv[1]
+    port = sys_argv[2]
+except IndexError as error:
+    LogWrite("System argument error: %s" % error, level=2)
+    debugger("%s - sys_argv error: %s" % (function, error))
+    raise SystemExit
+try: device_mapping_dict = dict(sys_argv[3])
+except IndexError:
+    device_mapping = False
+    debugger("%s - sys_argv not device_mapping_dict" % function)
+try: setting_dict = dict(sys_argv[4])
+except IndexError: debugger("%s - sys_argv not setting_dict" % function)
+
+
+class Device:
+    def __init__(self):
+        self.data = ""
+        if not device_mapping: self.output_dict = False
+        else: self.output_dict = True
+
+    def start(self):
+        debugger("%s - start |starting get_data")
+        self.get_data()
+        if self.output_dict: self.data_mapping()
+        print(self.data)
+        LogWrite("Data was delivered.", level=4)
+        debugger("%s - start |finished")
+        raise SystemExit
+
+    def data_mapping(self):
+        self.data = zip(device_mapping_dict.keys(), list(self.data))
+        debugger("%s - data_mapping |%s" % (function, self.data))
+
+    def get_data(self):
+        print("getting data")
+        self.data = "data"
+        # get data and either
+        #  split it into an list if multiple
+        #  or return the string if single
+
+
+Device().start()
