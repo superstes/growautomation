af2e526d1e17b4285e12d9d087aafa4bc560bce6
Rene Rath <rene.rath@growautomation.at>
2020-05-19 23:11:19 +0200

    replacing " with ', fixing connection test for user root in setup,

--- code/agent/core/owl.py
+++ code/agent/core/owl.py
@@ -25,6 +25,7 @@ from smallant import Log
 from smallconfig import Config
 from smallant import debugger
 from smallant import process
+from random import choice as random_choice
 
 from os import path as os_path
 from time import sleep as time_sleep
@@ -34,47 +35,49 @@ from functools import lru_cache
 class DoSql:
     def __init__(self, command, write=False, user=None, pwd=None, exit=True, hostname=None, setuptype=None):
         self.command, self.write, self.user, self.pwd, self.exit = command, write, user, pwd, exit
+        if hostname is not None and setuptype is not None: self.setup = True
+        else: self.setup = False
         if hostname is None:
-            self.hostname = Config("hostname").get()
+            self.hostname = Config('hostname').get()
         else: self.hostname = hostname
         if setuptype is None:
-            self.setuptype = Config("setuptype").get()
+            self.setuptype = Config('setuptype').get()
         else: self.setuptype = setuptype
         self.fallback = False
 
     def start(self):
         def systemd_check(typ):
-            mysql_status = process("systemctl status mysql.service")
-            if typ == "ok":
+            mysql_status = process('systemctl status mysql.service')
+            if typ == 'ok':
                 if mysql_status.find("active (running)") != -1: return True
                 else: return False
-            elif typ == "dead":
-                if mysql_status.find("failed") != -1 or mysql_status.find("could not be found") != -1:
+            elif typ == 'dead':
+                if mysql_status.find('failed') != -1 or mysql_status.find('could not be found') != -1:
                     return True
                 else: return False
         whilecount, conntest_result = 0, False
         while True:
-            if systemd_check("ok") is False:
+            if systemd_check('ok') is False:
                 if whilecount > 0:
-                    if systemd_check("dead") is True: return False
-                debugger("owl - start |mysql not running")
-                process("systemctl start mysql.service")
+                    if systemd_check('dead') is True: return False
+                debugger('owl - start |mysql not running')
+                process('systemctl start mysql.service')
             else: break
             if whilecount > 2: break
             time_sleep(.5)
             whilecount += 1
         whilecount = 0
         while conntest_result is False:
-            if whilecount == 1 and self.setuptype == "agent":
-                debugger("owl - start |failing over to local db")
-                Log("Failing over to local read-only database").write()
+            if whilecount == 1 and self.setuptype == 'agent':
+                debugger('owl - start |failing over to local db')
+                Log('Failing over to local read-only database').write()
                 self.fallback = True
             if self.fallback is True and self.write is True:
-                Log("Error connecting to database. Write operations are not allowed to local fallback database. "
-                    "Check you sql server connection.").write()
+                Log('Error connecting to database. Write operations are not allowed to local fallback database. '
+                    'Check you sql server connection.').write()
                 if self.exit:
-                    raise SystemExit("Error connecting to database. Write operations are not allowed to local fallback database. "
-                                     "Check you sql server connection.")
+                    raise SystemExit('Error connecting to database. Write operations are not allowed to local fallback database. '
+                                     'Check you sql server connection.')
                 else: return False
 
             if whilecount > 2:
@@ -83,13 +86,22 @@ class DoSql:
                     raise SystemExit("Error connecting to database. Check content of %ga_root/core/core.conf file for correct sql login credentials.")
                 else: return False
 
-            if self.write is False: data = self._connect("SELECT * FROM mysql.help_category LIMIT 10;", connect_debug=False)
+            if self.write is False: data = self._connect('SELECT * FROM mysql.help_category LIMIT 10;', connect_debug=False)
             else:
-                self._connect("INSERT INTO ga.Setting (author, belonging, setting, data) VALUES ('owl', '%s', 'conntest', 'ok');" % self.hostname, connect_debug=False)
-                self._connect("DELETE FROM ga.Setting WHERE author = 'owl' and belonging = '%s';" % self.hostname, connect_debug=False)
-                data = True
-            conntest_result = True if type(data) == list else data if type(data) == bool else False
-            debugger("owl - start |conntest '%s' '%s'" % (type(conntest_result), conntest_result))
+                if self.setup and self.user == 'root':
+                    rand_db_nr = ''.join(random_choice('0123456789') for _ in range(20))
+                    self._connect("CREATE DATABASE test_%s;" % rand_db_nr, connect_debug=False)
+                    data = self._connect("DROP DATABASE test_%s;" % rand_db_nr, connect_debug=False)
+                else:
+                    self._connect("INSERT INTO ga.Setting (author, belonging, setting, data) VALUES ('owl', '%s', 'conntest', 'ok');"
+                                  % self.hostname, connect_debug=False)
+                    data = self._connect("DELETE FROM ga.Setting WHERE author = 'owl' and belonging = '%s';"
+                                         % self.hostname, connect_debug=False)
+            debugger("owl - start |conntest output '%s' '%s'" % (type(data), data))
+            if type(data) == list or type(data) == str: conntest_result = True
+            elif type(data) == bool: conntest_result = data
+            else: conntest_result = False
+            debugger("owl - start |conntest result '%s' " % conntest_result)
             whilecount += 1
         return self._execute()
 
@@ -119,24 +131,24 @@ class DoSql:
         import mysql.connector
         try:
             if self.user is not None:
-                if self.user == "root":
+                if self.user == 'root':
                     connection = mysql.connector.connect(unix_socket=self._unixsock(), user=self.user)
                 else:
-                    if self.setuptype == "agent":
-                        connection = mysql.connector.connect(host="%s" % self._check_config("sql_server_ip"),
-                                                             port="%s" % self._check_config("sql_server_port"),
+                    if self.setuptype == 'agent':
+                        connection = mysql.connector.connect(host="%s" % self._check_config('sql_server_ip'),
+                                                             port="%s" % self._check_config('sql_server_port'),
                                                              user=self.user, passwd=self.pwd)
                     else:
                         connection = mysql.connector.connect(unix_socket=self._unixsock(), user=self.user, passwd=self.pwd)
-            elif self.setuptype == "agent":
-                if self.fallback is True: connection = mysql.connector.connect(user="%s" % self._check_config("sql_local_user"),
-                                                                               passwd="%s" % self._check_config("sql_local_pwd"))
-                else: connection = mysql.connector.connect(unix_socket=self._unixsock(), host="%s" % self._check_config("sql_server_ip"),
-                                                           port="%s" % self._check_config("sql_server_port"),
-                                                           user="%s" % self._check_config("sql_agent_user"),
-                                                           passwd="%s" % self._check_config("sql_agent_pwd"))
-            else: connection = mysql.connector.connect(unix_socket=self._unixsock(), user="%s" % self._check_config("sql_admin_user"),
-                                                       passwd="%s" % self._check_config("sql_admin_pwd"))
+            elif self.setuptype == 'agent':
+                if self.fallback is True: connection = mysql.connector.connect(user="%s" % self._check_config('sql_local_user'),
+                                                                               passwd="%s" % self._check_config('sql_local_pwd'))
+                else: connection = mysql.connector.connect(unix_socket=self._unixsock(), host="%s" % self._check_config('sql_server_ip'),
+                                                           port="%s" % self._check_config('sql_server_port'),
+                                                           user="%s" % self._check_config('sql_agent_user'),
+                                                           passwd="%s" % self._check_config('sql_agent_pwd'))
+            else: connection = mysql.connector.connect(unix_socket=self._unixsock(), user="%s" % self._check_config('sql_admin_user'),
+                                                       passwd="%s" % self._check_config('sql_admin_pwd'))
             cursor = connection.cursor(buffered=True)
             if command is None: command = self.command
             if connect_debug: debugger("owl - connect |command '%s' '%s'" % (type(command), command))
@@ -162,7 +174,7 @@ class DoSql:
             if connect_debug: debugger("owl - connect |output '%s' '%s'" % (type(data), data))
             return data
         except (mysql.connector.Error, mysql.connector.errors.InterfaceError, ValueError) as error:
-            if connect_debug: debugger("owl - connect |error '%s'" % error)
+            debugger("owl - connect |error '%s'" % error)
             try:
                 connection.rollback()
             except UnboundLocalError: pass
@@ -171,17 +183,17 @@ class DoSql:
 
     def _unixsock(self):
         try:
-            sql_sock = process(command="cat /etc/mysql/mariadb.conf.d/50-server.cnf | grep 'socket                  ='").split("=")[1].strip()
+            sql_sock = process(command="cat /etc/mysql/mariadb.conf.d/50-server.cnf | grep 'socket                  ='").split('=')[1].strip()
             if os_path.exists(sql_sock) is False:
-                if process(command="systemctl status mysql.service | grep 'Active:'").find("Active: inactive") != -1:
-                    if process("systemctl start mysql.service").find("Not able to start") != -1:
+                if process(command="systemctl status mysql.service | grep 'Active:'").find('Active: inactive') != -1:
+                    if process('systemctl start mysql.service').find('Not able to start') != -1:
                         return False
                     time_sleep(5)
             return sql_sock
         except IndexError: return False
 
     def find(self, searchfor):
-        debugger("owl - find |input '%s' '%s'" % (type(searchfor), searchfor))
+        debugger("owl - find |input '%s' '%s' '%s'" % (type(searchfor), searchfor, self.command))
         if type(self.command) == str:
             data = str(self._execute())
             output = data.find(searchfor)
@@ -193,19 +205,20 @@ class DoSql:
         return output
 
 
-def sql_replace(data_dict, table="setting", debug=False):
+def sql_replace(data_dict, table='setting', debug=False):
     # first entry in data_dict = value that should be changed
     debugger("owl - replace |input dict '%s'" % data_dict, hard_debug=debug)
-    if table == "setting": table = "ga.Setting"
-    elif table == "group": table = "ga.Member"
-    elif table == "object": table = "ga.Object"
-    elif table == "data": table = "ga.Data"
-    elif table == "tmp": table = "ga.Temp"
+    if table == 'setting': table = 'ga.Setting'
+    elif table == 'grp': table = 'ga.Grp'
+    elif table == 'member': table = 'ga.Member'
+    elif table == 'object': table = 'ga.Object'
+    elif table == 'data': table = 'ga.Data'
+    elif table == 'tmp': table = 'ga.Temp'
     else: return False
     command, entrycount = [], 0
     for key, value in data_dict.items():
         append_str = []
-        if entrycount > 1: append_str.append("AND")
+        if entrycount > 1: append_str.append('AND')
         if entrycount > 0:
             append_str.append("%s = '%s'" % (key, value))
             command.append(' '.join(append_str))
