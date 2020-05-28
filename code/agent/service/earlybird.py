fa26c166ae5402ed7d8eaf0c39de117929443514
Rene Rath <rene.rath@growautomation.at>
2020-05-28 02:57:21 +0200

    updated and activated earlybird service-pre-checks

--- code/agent/service/earlybird.py
+++ code/agent/service/earlybird.py
@@ -29,59 +29,91 @@ from core.smallant import Log
 from systemd import journal as systemd_journal
 from sys import argv as sys_argv
 from sys import exc_info as sys_exc_info
+from os import system as os_system
 import signal
 
 
-class Startup:
+class Prepare:
     def __init__(self):
-        signal.signal(signal.SIGTERM, self.stop)
-        signal.signal(signal.SIGINT, self.stop)
-#        self.start()
-#        not finished yet
+        signal.signal(signal.SIGTERM, self._stop)
+        signal.signal(signal.SIGINT, self._stop)
+        self.debug = False
 
     def start(self):
+        VarHandler(name='init', data=1).set()
         try:
-            if sys_argv[1] == 'debug': VarHandler(name='debug', data=1).set()
+            if sys_argv[1] == 'debug':
+                VarHandler(name='debug', data=1).set()
+                self.debug = True
         except (IndexError, NameError): pass
-        systemd_journal('Starting service initialization.')
+        systemd_journal.write('Starting service initialization.')
+        self._var_cleanup()
+        self._db_connection_test()
+        self._config_update()
+        self._finish()
         # recreate log/backup links
-        # check for python version -> module link should be updated
+        # check python version -> for future dynamic python version usage
 
-    def stop(self, signum=None, stack=None):
+    def _stop(self, signum=None, stack=None):
         if signum is not None:
             try:
-                debugger("service - stop |got signal %s - '%s'" % (signum, sys_exc_info()[0].__name__))
-                Log("Service received signal %s - '%s'" % (signum, sys_exc_info()[0].__name__), level=2).write()
+                debugger("earlybird - stop |got signal %s - '%s'" % (signum, sys_exc_info()[0].__name__))
+                Log("Service initiation received signal %s - '%s'" % (signum, sys_exc_info()[0].__name__), level=2).write()
             except AttributeError:
-                debugger("service - stop |got signal %s" % signum)
-                Log("Service received signal %s" % signum, level=2).write()
-        systemd_journal('Service initialization stopped. Exiting.')
+                debugger("earlybird - stop |got signal %s" % signum)
+                Log("Service initiation received signal %s" % signum, level=2).write()
+        debugger('earlybird - stop |received error - exiting')
+        systemd_journal.write('Service initiation initialization stopped. Exiting.')
+        VarHandler().stop()
 
-    def finish(self):
-        try:
-            if sys_argv[1] == 'debug': VarHandler(name='debug').clean()
-        except (IndexError, NameError): pass
+    def _finish(self):
+        debugger('earlybird - finish |finished service preparations')
+        systemd_journal.write('Finished service initialization.')
+        VarHandler().stop()
 
-    def config(self):
-        return False
-        # get config from db
-        # check against locally written config files (core.conf/version file)
+    def _config_update(self):
+        def update(file):
+            file_dict, db_dict = {}, {}
+            with open(file, 'r') as config_file:
+                line_list = config_file.readlines()
+                for line in line_list:
+                    if line.find('=') != -1:
+                        _ = line.split('=', 1)
+                        file_dict[_[0]] = _[1].strip()
+            for setting, file_data in file_dict.items():
+                db_data = Config(setting=setting).get()
+                if db_data is not False:
+                    if db_data != file_data:
+                        os_system("sed -i 's$%s$%s$g' %s" % ("%s=%s" % (setting, file_data), "%s=%s" % (setting, db_data), file))
+                        debugger("earlybird - config_update |updated setting '%s' in file '%s'" % (setting, file))
+                        Log("Updated setting '%s' data in config file '%s'" % (setting, file))
+                else:
+                    debugger("earlybird - config_update |setting '%s' not found in db" % setting)
+                    continue
+        update(file="%s/core/core.conf" % Config(setting='path_root').get())
+        update(file='/etc/growautomation.version')
 
-    def db(self):
-        sql = DoSql()
+    def _db_connection_test(self):
+        error = False
+        if DoSql(test=True, exit=False).start() is False:
+            debugger('earlybird - db |read - connection error')
+            Log('Cannot read from growautomation database', level=1)
+            error = True
+        if DoSql(test=True, write=True, exit=False).start() is False:
+            debugger('earlybird - db |write - connection error')
+            Log('Cannot write to growautomation database', level=1)
+            error = True
+        if error:
+            systemd_journal.write('Error while testing growautomation database connection')
 
-        def db_read_test():
-            data = sql.connect('SELECT * FROM ga.Setting ORDER BY changed DESC LIMIT 10;')
-            result = True if type(data) == list else data if type(data) == bool else False
-            return result
+    def _var_cleanup(self):
+        def clean_if_exists(name):
+            if VarHandler(name=name).get() is not False:
+                VarHandler(name=name).clean()
 
-        def db_write_test():
-            sql.connect("INSERT INTO ga.Setting (author, belonging, setting, data) VALUES ('owl', '%s', 'conntest', 'ok');" % Config('hostname').get())
-            sql.connect("DELETE FROM ga.Setting WHERE author = 'owl' and belonging = '%s';" % Config('hostname').get())
-            return True
-        if db_read_test() is True and db_write_test() is True:
-            DoSql('DELETE FROM ga.Temp;', write=True).start()
-            # check that no locks are set -> set all to 0 or simply remove them (or remove all entries from temp table ?!)
+        [clean_if_exists('lock_%s' % _) for _ in Config(output='name', table='object').get()]
+        if not self.debug: clean_if_exists('debug')
+        clean_if_exists('service_stop')
 
 
-Startup()
+Prepare().start()
