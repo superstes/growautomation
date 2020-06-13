a4c92ee8c9624bcc59b87c87c8744610526d4876
Rene Rath <rene.rath@growautomation.at>
2020-06-14 01:31:05 +0200

    removed table 'Reference', added table 'GrpSetting', added columns to table 'Profile', renamed foreign and unique keys

--- setup/server/ga_db_setup.sql
+++ setup/server/ga_db_setup.sql
@@ -12,17 +12,6 @@ create table IF NOT EXISTS Data (
  character set utf8,
  collate utf8_unicode_ci;
 
-create table IF NOT EXISTS Reference (
-	id smallint unsigned not null auto_increment,
-	changed timestamp not null default current_timestamp on update current_timestamp,
-	author varchar(20) not null,
-	name varchar(30) not null unique key,
-	description varchar(50) null,
-	primary key (id)
-)engine innodb,
- character set utf8,
- collate utf8_unicode_ci;
-
 create table IF NOT EXISTS Object (
 	id smallint unsigned not null auto_increment,
 	changed timestamp not null default current_timestamp on update current_timestamp,
@@ -32,10 +21,7 @@ create table IF NOT EXISTS Object (
 	class varchar(20) default null,
 	type varchar(30) not null,
 	description varchar(50) null,
-	primary key (id),
-	foreign key (class) references Reference (name) on update cascade on delete cascade,
-	foreign key (parent) references Reference (name) on update cascade on delete cascade,
-	foreign key (type) references Reference (name) on update cascade on delete cascade
+	primary key (id)
 )engine innodb,
  character set utf8,
  collate utf8_unicode_ci;
@@ -49,8 +35,8 @@ create table IF NOT EXISTS Setting (
 	data varchar(100) not null,
 	description varchar(50) null,
     primary key (id),
-    foreign key (belonging) references Object (name) on update cascade on delete cascade,
-    unique key unique_belonging_setting (belonging, setting)
+    foreign key 3_fk_object_name (belonging) references Object (name) on update cascade on delete cascade,
+    unique key 3_uk_belonging_setting (belonging, setting)
 )engine innodb,
  character set utf8,
  collate utf8_unicode_ci;
@@ -64,8 +50,7 @@ create table IF NOT EXISTS Grp (
     parent smallint unsigned null default null,
 	description varchar(50) null default null,
     primary key (id),
-    foreign key (type) references Reference (name) on update cascade on delete cascade,
-    unique key (name)
+    unique key 4_uk_name (name)
 )engine innodb,
  character set utf8,
  collate utf8_unicode_ci;
@@ -78,9 +63,24 @@ create table IF NOT EXISTS Member (
     member varchar(20) not null,
 	description varchar(50) null,
     primary key (id),
-    foreign key (member) references Object (name) on update cascade on delete cascade,
-    foreign key (gid) references Grp (id) on update cascade on delete cascade,
-    unique key unique_gid_member (gid, member)
+    foreign key 5_fk_object_name (member) references Object (name) on update cascade on delete cascade,
+    foreign key 5_fk_grp_id (gid) references Grp (id) on update cascade on delete cascade,
+    unique key 5_uk_gid_member (gid, member)
+)engine innodb,
+ character set utf8,
+ collate utf8_unicode_ci;
+
+create table IF NOT EXISTS GrpSetting (
+	id smallint unsigned not null auto_increment,
+	changed timestamp not null default current_timestamp on update current_timestamp,
+	author varchar(20) not null,
+	belonging varchar(20) not null,
+    setting varchar(30) not null,
+	data varchar(100) not null,
+	description varchar(50) null,
+    primary key (id),
+    foreign key 8_fk_grp_name (belonging) references Grp (name) on update cascade on delete cascade,
+    unique key 8_uk_belonging_setting (belonging, setting)
 )engine innodb,
  character set utf8,
  collate utf8_unicode_ci;
@@ -97,8 +97,9 @@ create table IF NOT EXISTS ProfileGrp (
 	name varchar(20) not null,
 	description varchar(50) null default null,
     primary key (id),
-    foreign key (gid) references Grp (id) on update cascade on delete cascade,
-    unique key unique_gid_parent_id (gid, parent_id)
+    foreign key 6_fk_grp_id (gid) references Grp (id) on update cascade on delete cascade,
+    unique key 6_uk_parent_id (parent_id),
+    unique key 6_uk_gid_name (gid, name)
 )engine innodb,
  character set utf8,
  collate utf8_unicode_ci;
@@ -111,15 +112,20 @@ create table IF NOT EXISTS Profile (
 	parent smallint unsigned null default null,
 	gid smallint unsigned not null,
 	object varchar(20) not null,
+	sector varchar(20) null default null,
+	data_source varchar(20) not null,
+	data_points smallint unsigned null default null,
 	threshold varchar(20) not null,
 	condi varchar(10) not null,
 	operator varchar(10) null default null,
 	name varchar(20) not null,
 	description varchar(50) null default null,
     primary key (id),
---    foreign key (parent) references ProfileGrp (parent_id) on update cascade on delete cascade,
-    foreign key (object) references Object (name) on update cascade on delete cascade,
-    foreign key (gid) references Grp (id) on update cascade on delete cascade
+    foreign key 7_fk_profilegrp_parent (parent) references ProfileGrp (parent_id) on update cascade on delete cascade,
+    foreign key 7_fk_object_name (object) references Object (name) on update cascade on delete cascade,
+    foreign key 7_fk_grp_name (sector) references Grp (name) on update cascade on delete cascade,
+    foreign key 7_fk_grp_id (gid) references Grp (id) on update cascade on delete cascade,
+    unique key 7_uk_gid_name (gid, name)
 )engine innodb,
  character set utf8,
  collate utf8_unicode_ci;
