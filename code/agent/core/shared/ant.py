1f8aa6839ddcecdcaa80e7d6fa421219809c273d
Rene Rath <rene.rath@growautomation.at>
2020-06-16 22:30:24 +0200

    moved debugger, process and internal_process functions to ant

--- code/agent/core/shared/ant.py
+++ code/agent/core/shared/ant.py
@@ -24,14 +24,15 @@
 
 try:
     from core.config import Config
+    import core.shared.smallant
 except (ImportError, ModuleNotFoundError):
     from config import Config
-import smallant
+    import smallant
 
 from os import system as os_system
+from time import sleep as time_sleep
 
 
-# File operations
 class Line:
     def __init__(self, action, search, replace='', backup=False, file='./core.conf'):
         self.file, self.backup, self.searchfor, self.action, self.replacewith = file, backup, search, action, replace
@@ -58,3 +59,61 @@ class Line:
         # insert after linenr / search = linenr
         os_system("sed -i%s '%s a %s' %s && mv %s %s %s" % (self.backupfile, self.searchfor, self.replacewith, self.file, self.file, self.backupfile, self.backupdir)) if self.backup == 'yes' \
             else os_system("sed -i '%s a %s' %s" % (self.searchfor, self.replacewith, self.file))
+
+
+def debugger(command, hard_debug=False, hard_only=False, level=1):
+    if level > 1: return False
+    if hard_debug: debug = True
+    elif not hard_only:
+        try:
+            from core.shared.varhandler import VarHandler
+        except (ImportError, ModuleNotFoundError):
+            from varhandler import VarHandler
+        debug = True if VarHandler(name='debug').get() == '1' else False
+    else: debug = False
+    if debug is True:
+        prefix = "%s debug:" % smallant.now("%H:%M:%S")
+        if type(command) == str:
+            print(prefix, command)
+        elif type(command) == list:
+            [print(prefix, call) for call in command]
+        return True
+    else: return False
+
+
+def process(command, out_error=False, debug=False):
+    from subprocess import Popen as subprocess_popen
+    from subprocess import PIPE as subprocess_pipe
+    if debug: debugger(command="smallant - process |input: '%s'" % command, hard_debug=True)
+    output, error = subprocess_popen([command], shell=True, stdout=subprocess_pipe, stderr=subprocess_pipe).communicate()
+    output, error = output.decode('utf-8').strip(), error.decode('utf-8').strip()
+    if debug: debugger(command="smallant - process |output: '%s' '%s' |error: '%s' '%s'"
+                               % (type(output), output, type(error), error), hard_debug=True)
+    if out_error is False: return output
+    else: return output, error
+
+
+def internal_process(target, argument=None, stdout=True, debug=False):
+    from multiprocessing import Process as MP_Process
+    from multiprocessing import Queue as MP_Queue
+    if debug: debugger(command="smallant - internal_process |input: '%s' '%s'" % (type(target), target.__name__), hard_debug=True)
+    stdout_pipe = MP_Queue()
+    if stdout is True: _process = MP_Process(target=target, args=(argument, stdout_pipe))
+    else: _process = MP_Process(target=target, args=argument)
+    _process.start()
+    runtime_count, finished = 1, False
+    while True:
+        if not _process.is_alive():
+            _process.join()
+            if stdout is True:
+                output = stdout_pipe.get()
+                if debug: debugger(command="smallant - internal_process |output: '%s' '%s'" % (type(output), output), hard_debug=True)
+                return output
+            return True
+        elif runtime_count > 5:
+            _process.terminate()
+            _process.join()
+            if debug: debugger(command="smallant - internal_process |timeout - terminated", hard_debug=True)
+            return False
+        else: time_sleep(3)
+        runtime_count += 1
--- code/agent/core/shared/smallant.py
+++ code/agent/core/shared/smallant.py
@@ -26,12 +26,10 @@ try:
     from core.smallconfig import Config
 except (ImportError, ModuleNotFoundError):
     from smallconfig import Config
-from varhandler import VarHandler
 
 from os import system as os_system
 from os import path as os_path
 from datetime import datetime
-from time import sleep as time_sleep
 from random import choice as random_choice
 
 
@@ -73,60 +71,6 @@ class Log:
         if os_path.exists(self.log_dir) is False: os_system("mkdir -p %s" % self.log_dir)
         if os_path.exists(self.log_file) is False: os_system("touch %s" % self.log_file)
         return self.log_file
-
-
-def debugger(command, hard_debug=False, hard_only=False, level=1):
-    if level > 1: return False
-    if hard_debug: debug = True
-    elif not hard_only:
-        debug = True if VarHandler(name='debug').get() == '1' else False
-    else: debug = False
-    if debug is True:
-        prefix = "%s debug:" % now("%H:%M:%S")
-        if type(command) == str:
-            print(prefix, command)
-        elif type(command) == list:
-            [print(prefix, call) for call in command]
-        return True
-    else: return False
-
-
-def process(command, out_error=False, debug=False):
-    from subprocess import Popen as subprocess_popen
-    from subprocess import PIPE as subprocess_pipe
-    if debug: debugger(command="smallant - process |input: '%s'" % command, hard_debug=True)
-    output, error = subprocess_popen([command], shell=True, stdout=subprocess_pipe, stderr=subprocess_pipe).communicate()
-    output, error = output.decode('utf-8').strip(), error.decode('utf-8').strip()
-    if debug: debugger(command="smallant - process |output: '%s' '%s' |error: '%s' '%s'"
-                               % (type(output), output, type(error), error), hard_debug=True)
-    if out_error is False: return output
-    else: return output, error
-
-
-def internal_process(target, argument=None, stdout=True, debug=False):
-    from multiprocessing import Process as MP_Process
-    from multiprocessing import Queue as MP_Queue
-    if debug: debugger(command="smallant - internal_process |input: '%s' '%s'" % (type(target), target.__name__), hard_debug=True)
-    stdout_pipe = MP_Queue()
-    if stdout is True: _process = MP_Process(target=target, args=(argument, stdout_pipe))
-    else: _process = MP_Process(target=target, args=argument)
-    _process.start()
-    runtime_count, finished = 1, False
-    while True:
-        if not _process.is_alive():
-            _process.join()
-            if stdout is True:
-                output = stdout_pipe.get()
-                if debug: debugger(command="smallant - internal_process |output: '%s' '%s'" % (type(output), output), hard_debug=True)
-                return output
-            return True
-        elif runtime_count > 5:
-            _process.terminate()
-            _process.join()
-            if debug: debugger(command="smallant - internal_process |timeout - terminated", hard_debug=True)
-            return False
-        else: time_sleep(3)
-        runtime_count += 1
 
 
 def list_remove_duplicates(_list):
