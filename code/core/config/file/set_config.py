a50b6971653421e8ba540bcf8e17fff94b820b36
Rene Rath <rene.rath@growautomation.at>
2020-11-05 20:02:36 +0100

    v0.6 optimized output conditions; cleaned up factory

--- code/core/config/file/set_config.py
+++
@@ -1,8 +0,0 @@
-from core.config.file.reset import go as reset
-from core.config.file.get import go as get
-
-file = '/etc/ga/core/config/file/core.conf'
-
-reset(file, {'sql_server': '127.0.0.1', 'sql_port': '3306', 'sql_user': 'test', 'sql_secret': '789TMP01!', 'sql_database': 'ga'}, encrypted=False)
-
-print(get(file, encrypted=False))
