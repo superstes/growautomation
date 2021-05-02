0f71b2ab3b0c2cfb737e52018f5f5f5c424c6655
Rene Rath <rene.rath@superstes.eu>
2021-05-02 08:42:28 +0200

    web - added nested dropdown to navigation bar

--- code/web/base/ga/config/nav.py
+++ code/web/base/ga/config/nav.py
@@ -19,17 +19,19 @@ nav_dict = {
             'Service': '/system/service/',  # status and restart of service(s)
             'Logs': '/system/log/',  # read various log files
             'Scripts': '/system/script/',  # upload or delete scripts
-            'Config Export': '/system/export/config/',  # download db dump of config
-            'Data Export': '/system/export/data/',  # download db dump of data
-            'Full Export': '/system/export/full/',  # download db dump of all ga tables
+            'Export': {
+                'Config': '/system/export/config/',   # download db dump of config
+                'Data': '/system/export/data/',  # download db dump of data
+                'Full': '/system/export/full/',  # download db dump of all ga tables
+            },
         },
     },
     'right': {
-        '<i class="fab fa-youtube fa-2x ga-icon-nav" title="YouTube"></i>': 'https://www.youtube.com/channel/UCLJyDlo3Z6eP_X2Pw0-Z8Pw',
-        '<i class="fab fa-github-square fa-2x ga-icon-nav" title="GitHub"></i>': 'https://github.com/superstes/growautomation',
-        '<i class="fas fa-coins fa-2x ga-icon-nav" title="Donate"></i>': 'https://www.patreon.com/growautomation/membership',
-        '<i class="fas fa-bug fa-2x ga-icon-nav" title="Report bugs"></i>': 'https://docs.growautomation.eu/en/latest/basic/bugs.html',
-        '<i class="fas fa-book fa-2x ga-icon-nav" title="Documentation"></i>': 'https://docs.growautomation.eu',
-        '<i class="fas fa-sign-out-alt fa-2x ga-icon-nav ga-icon-logout" title="Logout"></i>': '/logout/',
+        '<i class="fab fa-youtube fa-2x ga-nav-right-icon" title="YouTube"></i>': 'https://www.youtube.com/channel/UCLJyDlo3Z6eP_X2Pw0-Z8Pw',
+        '<i class="fab fa-github-square fa-2x ga-nav-right-icon" title="GitHub"></i>': 'https://github.com/superstes/growautomation',
+        '<i class="fas fa-coins fa-2x ga-nav-right-icon" title="Donate"></i>': 'https://www.patreon.com/growautomation/membership',
+        '<i class="fas fa-bug fa-2x ga-nav-right-icon" title="Report bugs"></i>': 'https://docs.growautomation.eu/en/latest/basic/bugs.html',
+        '<i class="fas fa-book fa-2x ga-nav-right-icon" title="Documentation"></i>': 'https://docs.growautomation.eu',
+        '<i class="fas fa-sign-out-alt fa-2x ga-nav-right-icon ga-nav-right-icon-logout" title="Logout"></i>': '/logout/',
     }
 }
