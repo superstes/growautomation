b539f2b57407d78cce2e79bd759b23a2b6325597
René Rath <rene.rath@superstes.eu>
2021-09-23 17:30:04 +0200

    web - updated bug link

--- code/web/base/ga/config/nav.py
+++ code/web/base/ga/config/nav.py
@@ -30,7 +30,7 @@ nav_dict = {
         '<i class="fab fa-youtube fa-2x ga-nav-right-icon" title="YouTube"></i>': 'https://www.youtube.com/channel/UCLJyDlo3Z6eP_X2Pw0-Z8Pw',
         '<i class="fab fa-github-square fa-2x ga-nav-right-icon" title="GitHub"></i>': 'https://github.com/superstes/growautomation',
         '<i class="fas fa-coins fa-2x ga-nav-right-icon" title="Donate"></i>': 'https://www.patreon.com/growautomation/membership',
-        '<i class="fas fa-bug fa-2x ga-nav-right-icon" title="Report bugs"></i>': 'https://docs.growautomation.eu/en/latest/basic/bugs.html',
+        '<i class="fas fa-bug fa-2x ga-nav-right-icon" title="Report bug"></i>': 'https://github.com/superstes/growautomation/issues/new',
         '<i class="fas fa-book fa-2x ga-nav-right-icon" title="Documentation"></i>': 'https://docs.growautomation.eu',
         '<i class="fas fa-sign-out-alt fa-2x ga-nav-right-icon ga-nav-right-icon-logout" title="Logout"></i>': '/logout/',
     }
