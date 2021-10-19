56054249e43d7de8a05e95810d70a9cc0f52a20a
René Rath <rene.rath@superstes.eu>
2021-10-19 17:48:09 +0200

    web - added manual switch for dark-mode

--- code/web/base/ga/config/nav.py
+++ code/web/base/ga/config/nav.py
@@ -27,11 +27,35 @@ NAVIGATION = {
         },
     },
     'right': {
-        '<i class="fab fa-youtube fa-2x ga-nav-right-icon" title="YouTube"></i>': 'https://www.youtube.com/channel/UCLJyDlo3Z6eP_X2Pw0-Z8Pw',
-        '<i class="fab fa-github-square fa-2x ga-nav-right-icon" title="GitHub"></i>': 'https://github.com/superstes/growautomation',
-        '<i class="fas fa-coins fa-2x ga-nav-right-icon" title="Donate"></i>': 'https://www.patreon.com/growautomation/membership',
-        '<i class="fas fa-bug fa-2x ga-nav-right-icon" title="Report bug"></i>': 'https://github.com/superstes/growautomation/issues/new',
-        '<i class="fas fa-book fa-2x ga-nav-right-icon" title="Documentation"></i>': 'https://docs.growautomation.eu',
-        '<i class="fas fa-sign-out-alt fa-2x ga-nav-right-icon ga-nav-right-icon-logout" title="Logout"></i>': '/logout/',
+        'YT': {
+            'element': '<i class="fab fa-youtube fa-2x ga-nav-right-icon" title="YouTube"></i>',
+            'url': 'https://www.youtube.com/channel/UCLJyDlo3Z6eP_X2Pw0-Z8Pw',
+            'login': False,
+        },
+        'GH': {
+            'element': '<i class="fab fa-github-square fa-2x ga-nav-right-icon" title="GitHub"></i>',
+            'url': 'https://github.com/superstes/growautomation',
+            'login': False,
+        },
+        'PT': {
+            'element': '<i class="fas fa-coins fa-2x ga-nav-right-icon" title="Donate"></i>',
+            'url': 'https://www.patreon.com/growautomation/membership',
+            'login': False,
+        },
+        'BUG': {
+            'element': '<i class="fas fa-bug fa-2x ga-nav-right-icon" title="Report bug"></i>',
+            'url': 'https://github.com/superstes/growautomation/issues/new',
+            'login': False,
+        },
+        'DOC': {
+            'element': '<i class="fas fa-book fa-2x ga-nav-right-icon" title="Documentation"></i>',
+            'url': 'https://docs.growautomation.eu',
+            'login': False,
+        },
+        'LO': {
+            'element': '<i class="fas fa-sign-out-alt fa-2x ga-nav-right-icon ga-nav-right-icon-logout" title="Logout"></i>',
+            'url': '/logout/',
+            'login': True,
+        },
     }
 }
